be5619282fe97a13b31dd39ec9a2320924143ef9
James Rhodes <james.rhodes@hotmail.com.au>
2022-06-22 18:01:36 +1000

    Removed the character controller class as it was unnecessary

--- src/CharacterFiles/Capybara.cpp
+++ src/CharacterFiles/Capybara.cpp
@@ -2,7 +2,8 @@
 
 void Capy::Update()
 {
-    controller.Update(physBody);
+    // controller.Update(physBody);
+    UpdateController();
     pos = GetPosition();
 }
 
@@ -15,4 +16,53 @@ Vector2 Capy::GetPosition()
 {
     b2Vec2 tempPos = physBody->GetPosition();
     return {tempPos.x, tempPos.y};
+}
+
+void Capy::UpdateController()
+{
+
+    if (IsKeyDown(KEY_LEFT))
+    {
+        b2Vec2 currentPos = physBody->GetPosition();
+        physBody->SetTransform(currentPos + b2Vec2(-speed * GetFrameTime(), 0), 0);
+        b2Vec2 newVel = physBody->GetLinearVelocity().x < 0 ? b2Vec2(physBody->GetLinearVelocity().x, physBody->GetLinearVelocity().y) : b2Vec2(0, physBody->GetLinearVelocity().y);
+
+        physBody->SetLinearVelocity(newVel);
+
+        physBody->SetAwake(true);
+    }
+
+    if (IsKeyDown(KEY_RIGHT))
+    {
+        b2Vec2 currentPos = physBody->GetPosition();
+        physBody->SetTransform(currentPos + b2Vec2(speed * GetFrameTime(), 0), 0);
+
+        b2Vec2 newVel = physBody->GetLinearVelocity().x >= 0 ? b2Vec2(physBody->GetLinearVelocity().x, physBody->GetLinearVelocity().y) : b2Vec2(0, physBody->GetLinearVelocity().y);
+        physBody->SetLinearVelocity(newVel);
+        physBody->SetAwake(true);
+    }
+
+    if (IsKeyPressed(KEY_UP))
+
+    {
+        if (physBody->GetLinearVelocity().y == 0)
+        {
+            float gravity = physBody->GetWorld()->GetGravity().y;
+            float jumpForce = physBody->GetMass() * sqrt(jumpHeight * -2 * physBody->GetGravityScale() * gravity);
+            physBody->ApplyLinearImpulseToCenter(b2Vec2(0, jumpForce), true);
+        }
+    }
+
+    if (IsKeyPressed(KEY_M))
+    {
+        if (GetTime() - timeOfLastDash > dashRechargeTime)
+        {
+            float dashForce = 10 * physBody->GetMass();
+
+            physBody->ApplyLinearImpulseToCenter(b2Vec2(dashForce, 0), true);
+            timeOfLastDash = GetTime();
+        }
+    }
+
+    physBody->SetGravityScale(physBody->GetLinearVelocity().y < 0 ? 6 : 4);
 }
--- src/CharacterFiles/Capybara.h
+++ src/CharacterFiles/Capybara.h
@@ -3,77 +3,11 @@
 #include "raylib.h"
 #include <math.h>
 #include "../EngineFiles/ECS.h"
-#include "CharacterController.h"
 
-class CapyController : public CharacterController
-{
-public:
-    CapyController() = default;
-    CapyController(float _speed, float _jumpHeight) : speed(_speed), jumpHeight(_jumpHeight){};
-
-    CharacterStates Update(b2Body *physBody) override
-    {
-        CharacterStates state = STANDING_STILL;
-        if (IsKeyDown(KEY_LEFT))
-        {
-            b2Vec2 currentPos = physBody->GetPosition();
-            physBody->SetTransform(currentPos + b2Vec2(-speed * GetFrameTime(), 0), 0);
-            b2Vec2 newVel = physBody->GetLinearVelocity().x < 0 ? b2Vec2(physBody->GetLinearVelocity().x, physBody->GetLinearVelocity().y) : b2Vec2(0, physBody->GetLinearVelocity().y);
-
-            physBody->SetLinearVelocity(newVel);
-
-            physBody->SetAwake(true);
-            state = RUNNING_LEFT;
-        }
-
-        if (IsKeyDown(KEY_RIGHT))
-        {
-            b2Vec2 currentPos = physBody->GetPosition();
-            physBody->SetTransform(currentPos + b2Vec2(speed * GetFrameTime(), 0), 0);
-
-            b2Vec2 newVel = physBody->GetLinearVelocity().x >= 0 ? b2Vec2(physBody->GetLinearVelocity().x, physBody->GetLinearVelocity().y) : b2Vec2(0, physBody->GetLinearVelocity().y);
-            physBody->SetLinearVelocity(newVel);
-            physBody->SetAwake(true);
-            state = RUNNING_RIGHT;
-        }
-
-        if (IsKeyPressed(KEY_UP))
-
-        {
-            if (physBody->GetLinearVelocity().y == 0)
-            {
-                float gravity = physBody->GetWorld()->GetGravity().y;
-                float jumpForce = physBody->GetMass() * sqrt(jumpHeight * -2 * physBody->GetGravityScale() * gravity);
-                physBody->ApplyLinearImpulseToCenter(b2Vec2(0, jumpForce), true);
-                state = JUMPING;
-            }
-        }
-
-        if (IsKeyPressed(KEY_M))
-        {
-            if (GetTime() - timeOfLastDash > dashRechargeTime)
-            {
-                float dashForce = 10 * physBody->GetMass();
-
-                physBody->ApplyLinearImpulseToCenter(b2Vec2(dashForce, 0), true);
-                timeOfLastDash = GetTime();
-            }
-        }
-
-        physBody->SetGravityScale(physBody->GetLinearVelocity().y < 0 ? 6 : 4);
-
-        return state;
-    };
-
-    float speed = 1;
-    float jumpHeight = 1;
-    float timeOfLastDash = 0;
-    float dashRechargeTime = 0.8;
-};
 class Capy : public Entity
 {
 public:
-    Capy(Vector2 initPosition) : pos(initPosition), controller(speed, jumpHeight)
+    Capy(Vector2 initPosition) : pos(initPosition)
     {
     }
 
@@ -98,6 +32,8 @@ public:
 
     void Draw();
 
+    void UpdateController();
+
     Vector2 GetPosition();
 
     Vector2 pos;
@@ -105,6 +41,6 @@ public:
     float height = 0.5;
     float speed = 3;
     float jumpHeight = 3;
-
-    CapyController controller;
+    float timeOfLastDash = 0;
+    float dashRechargeTime = 0.8;
 };
