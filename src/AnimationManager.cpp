68b9c36a09dab55cf0f5bc8673da2a75c5b55a0b
James Rhodes <james.rhodes@hotmail.com.au>
2022-06-26 12:50:27 +1000

    Added error checking to the animation manager

--- src/AnimationManager.cpp
+++ src/AnimationManager.cpp
@@ -6,10 +6,21 @@ void AnimationManager::AddAnimation(std::string _state, std::vector<int> _frames
     {
         m_animMap[_state] = std::make_unique<AnimationInfo>(_frames, accumulatorLimit);
     }
+    else
+    {
+
+        throw std::invalid_argument("There is already an animation with that name registered.");
+        return;
+    }
 }
 
 Rectangle AnimationManager::GetTextureRectangle()
 {
+    if (m_animMap.find(currState) == m_animMap.end())
+    {
+        throw std::invalid_argument("Could not get texture rectangle because the current state of the animation manager has not been set to a valid animation");
+        return (Rectangle){0, 0, 0, 0};
+    }
     AnimationInfo *animInfo = m_animMap[currState].get();
     if (accumulator >= animInfo->accumulatorLimit)
     {
@@ -36,6 +47,11 @@ Rectangle AnimationManager::GetTextureRectangleAtFrame(int frame)
 }
 void AnimationManager::SetState(std::string _state, float valueToAddToAccumulator)
 {
+    if (m_animMap.find(_state) == m_animMap.end())
+    {
+        throw std::invalid_argument("The animation state you tried to use has not been initialised with the animation manager (ie. does not exist)");
+        return;
+    }
     if (!isLocked)
     {
         if (_state != currState)
@@ -51,6 +67,11 @@ void AnimationManager::SetState(std::string _state, float valueToAddToAccumulato
 }
 void AnimationManager::SetStateLock(std::string _state, float _lockTime)
 {
+    if (m_animMap.find(_state) == m_animMap.end())
+    {
+        throw std::invalid_argument("The animation state you tried to use has not been initialised with the animation manager (ie. does not exist)");
+        return;
+    }
     isLocked = true;
     lockStartTime = GetTime();
     lockTime = _lockTime;
