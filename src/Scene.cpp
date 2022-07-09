cfec5f9ebd307d7a19d6447496d97b8f4390998d
James Rhodes <james.rhodes@hotmail.com.au>
2022-07-09 13:00:50 +1000

    Created fly and orange scene components

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -10,9 +10,10 @@ void Scene::Run()
 void Scene::Init()
 {
     ecs.CreateEntity<Ground>("Ground", (Vector2){0, -2.8}, 10, 0.5, (Rectangle){0, 97, 32, 36});
+    ecs.CreateEntity<Orange>("Orange", (Vector2){1, 1}, 0.2);
     // ecs.CreateEntity<RockGround>("Ground2", (Vector2){3, 0}, 1.7, 0.5);
 
-    // ecs.CreateEntity<Ground>("Fly", (Vector2){-2, 2}, 0.3, 0.3);
+    ecs.CreateEntity<Fly>("Fly", (Vector2){-2, 2});
     // ecs.CreateEntity<Ground>("Fly1", (Vector2){-1, 2}, 0.1, 0.1);
 
     ecs.CreateEntity<Capy>("Capy", (Vector2){0, 0});
--- src/SceneFiles/SceneComponents.h
+++ src/SceneFiles/SceneComponents.h
@@ -50,7 +50,7 @@ public:
         config.height = height;
         config.isDynamic = false;
         config.restitution = 0;
-        config.friction = 1;
+        config.friction = friction;
 
         ecs->RegisterEntityAsPhysicsObject(id, config);
         // Below are the coords for the start pos for future reference
@@ -70,7 +70,6 @@ public:
             float speed = -8;
             b2Vec2 diff;
             diff.x = speed * GetFrameTime();
-            // physBody->SetTransform(physBody->GetPosition() + diff, 0);
             physBody->SetLinearVelocity(diff);
             physBody->SetAwake(true);
         }
@@ -78,70 +77,12 @@ public:
 
     void Draw()
     {
-        // DrawRectanglePro((Rectangle){pos.x, pos.y, width, height}, {width / 2, height / 2}, 0, DARKGREEN);
-        // DrawRectangleV(pos, {width, height}, BLACK);
 
-        // Texture2D texture = ecs->GetSpriteSheet();
         float alphaDetailOffset = 5.0 / 64.0f; // 5 pixel offset for some more detail that has alpha back ground (can be walked through)
 
         DrawTextureTiledWithinCamera(ecs->GetSpriteSheet(), srcRect, (Rectangle){pos.x - (width / 2), pos.y + (height / 2) + alphaDetailOffset, width, -(height + alphaDetailOffset)}, {0, 0}, 0, 64, RAYWHITE);
     }
 
-    // void DrawTextureTiledWithinCamera(Texture2D texture, Rectangle source, Rectangle dest, Vector2 origin, float rotation, float cameraZoom, Color tint)
-    // {
-    //     // Could probably be done better but this will do for now!
-
-    //     if ((texture.id <= 0) || (cameraZoom <= 0.0f))
-    //     {
-    //         return; // Wanna see a infinite loop?!...just delete this line!
-    //     }
-    //     if ((source.width == 0) || (source.height == 0))
-    //     {
-    //         return;
-    //     }
-    //     float invCameraZoom = 1 / cameraZoom;
-    //     float destWidthPixels = std::abs(dest.width * cameraZoom);
-    //     float destHeightPixels = std::abs(dest.height * cameraZoom);
-
-    //     int numXTiles = std::ceil(destWidthPixels / source.width);
-    //     int numYTiles = std::ceil(destHeightPixels / source.height);
-
-    //     float dx = source.width * invCameraZoom;
-    //     float dy = source.height * invCameraZoom;
-
-    //     Vector2 currentPos = {dest.x, dest.y};
-
-    //     Vector2 boundary = {dest.x + dest.width, dest.y + dest.height};
-
-    //     for (int y = -1; y < numYTiles; y++)
-    //     {
-    //         if (y == -1)
-    //         {
-    //             y = 0; // To make sure the loop runs at least once
-    //         }
-    //         currentPos.x = dest.x;
-    //         for (int x = -1; x < numXTiles; x++)
-    //         {
-    //             if (x == -1)
-    //             {
-    //                 x = 0; // To make sure the loop runs at least once
-    //             }
-    //             float remainderX = std::abs(boundary.x - currentPos.x);
-    //             float remainderY = std::abs(boundary.y - currentPos.y);
-    //             Rectangle tempSrc = source;
-    //             tempSrc.width = std::min(source.width, remainderX * cameraZoom);
-    //             tempSrc.height = std::min(source.height, remainderY * cameraZoom);
-
-    //             Rectangle tempDest = {currentPos.x, currentPos.y, std::min(dx, remainderX), -std::min(dy, remainderY)};
-    //             DrawTexturePro(texture, tempSrc, tempDest, origin, rotation, tint);
-    //             currentPos.x += dx;
-    //         }
-    //         currentPos.y -= dy;
-    //     }
-
-    //     // Tiling logic goes here
-    //     // DrawTexturePro(texture, source, dest, origin, rotation, RAYWHITE);
-    // }
     Vector2 pos;
     float width;
     float height;
@@ -150,62 +91,53 @@ public:
     float friction;
 };
 
-// class RockGround : public Entity
-// {
-// public:
-//     RockGround(Vector2 _pos, float _width, float _height) : pos(_pos), width(_width), height(_height){};
+class Fly : public Entity
+{
+public:
+    // Pos is in world coords
+    Fly(Vector2 _pos) : pos(_pos){};
 
-//     void Register()
-//     {
-//         ecs->RegisterEntityAsDrawable(id);
-//         RectanglePhysicsObjectConfig config;
+    void Register()
+    {
+        ecs->RegisterEntityAsDrawable(id);
+        RectanglePhysicsObjectConfig config;
 
-//         config.pos = pos;
-//         config.width = width;
-//         config.height = height;
-//         config.isDynamic = false;
-//         config.restitution = 0;
+        config.pos = pos;
+        config.width = width;
+        config.height = width;
+        config.isDynamic = false;
+        config.restitution = 0;
+        config.friction = 1;
 
-//         ecs->RegisterEntityAsPhysicsObject(id, config);
+        ecs->RegisterEntityAsPhysicsObject(id, config);
+    }
 
-//         animManager = AnimationManager(ecs->GetSpriteSheet(), 32, 102, 32, 32);
-//         animManager.AddAnimation("Static", {0});
-//         animManager.SetState("Static");
-//     }
+    void Update()
+    {
+        pos = {physBody->GetPosition().x, physBody->GetPosition().y};
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            shouldMove = !shouldMove;
+        }
 
-//     void Update()
-//     {
-//         pos = {physBody->GetPosition().x, physBody->GetPosition().y};
-//         if (IsKeyPressed(KEY_SPACE))
-//         {
-//             shouldMove = !shouldMove;
-//         }
+        if (shouldMove)
+        {
+            float speed = -8;
+            b2Vec2 diff;
+            diff.x = speed * GetFrameTime();
+            physBody->SetLinearVelocity(diff);
+            physBody->SetAwake(true);
+        }
+    };
 
-//         if (shouldMove)
-//         {
-//             float speed = -8;
-//             b2Vec2 diff;
-//             diff.x = speed * GetFrameTime();
-//             // physBody->SetTransform(physBody->GetPosition() + diff, 0);
-//             physBody->SetLinearVelocity(diff);
-//             physBody->SetAwake(true);
-//         }
-//     };
+    void Draw()
+    {
+        // DrawTextureTiledWithinCamera(ecs->GetSpriteSheet(), srcRect, (Rectangle){pos.x - (width / 2), pos.y + (height / 2) + alphaDetailOffset, width, -(height + alphaDetailOffset)}, {0, 0}, 0, 64, RAYWHITE);
+        DrawRectanglePro((Rectangle){pos.x, pos.y, width, width}, {width / 2, width / 2}, 0, BLACK);
+        // Will eventually need an animation Manager once I have drawn the fly sprite
+    }
 
-//     void Draw()
-//     {
-//         // DrawRectanglePro((Rectangle){pos.x, pos.y, width, height}, {width / 2, height / 2}, 0, DARKGREEN);
-//         // DrawRectangleV(pos, {width, height}, BLACK);
-
-//         Texture2D texture = ecs->GetSpriteSheet();
-//         Rectangle src = animManager.GetTextureRectangle();
-
-//         DrawTextureTiledWithinCamera(texture, src, (Rectangle){pos.x - (width / 2), pos.y + (height / 2), width, -(height)}, {0, 0}, 0, 64, RAYWHITE);
-//     }
-
-//     Vector2 pos;
-//     float width;
-//     float height;
-//     AnimationManager animManager;
-//     bool shouldMove = false;
-// };
+    Vector2 pos;
+    float width = 0.2;
+    bool shouldMove = false;
+};
