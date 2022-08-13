a1b6c5233831b626cad6879b7d202660f04e1abe
James Rhodes <james.rhodes@hotmail.com.au>
2022-08-13 23:26:33 +1000

    Added pragma statements around raygui.h includes so that the massive amount of warnings were stopped

--- Game.h
+++ Game.h
@@ -1,6 +1,10 @@
 #pragma once
 #define RAYGUI_IMPLEMENTATION
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wenum-compare"
 #include "raygui.h"
+#pragma GCC diagnostic pop
+
 #include "./src/Scene.h"
 
 class Game
